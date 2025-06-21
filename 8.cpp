e0cb85e1513baae30ad03e9af5d743f8537ebe7b
r3dh0ss4in <hossainltd.24@yahoo.com>
2025-06-22 02:28:54 +0600

    Initial commit

---
+++ 8.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+void func(int A[][2][2]) {
+    // Address of A[0][0][0];
+    cout << A << endl << *A << endl << &A[0][0] << endl;
+    cout << *(A[0][1]) << " " << *(A[0][1]+1) << endl; // 7 9
+    cout << *(A[0][3]) << " " << *(A[0][3]+1) << endl; // 6 1
+}
+
+int main() {
+    int C[3][2][2] = {
+        {
+            {2,5}, {7,9}
+        },
+        {
+            {3,4}, {6,1}
+        },
+        {
+            {0,8}, {11,13}
+        }
+    };
+    int (*P)[2][2] = C;
+    func(C);
+}
